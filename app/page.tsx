aef6419965738af3a2c14cfee253abd9f6823076
Juan Ramirez <juanpram2000@gmail.com>
2024-05-30 22:41:45 -0700

    feat: added some basic radio buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,18 @@
 
 import MapWrapper from "@/components/MapWrapper";
 import { Input } from "@/components/ui/input";
+import { Field, Label, Radio, RadioGroup } from "@headlessui/react";
 import { Switch } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { Bars3BottomLeftIcon } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
 
 const TAILWIND_SM_SCREEN: number = 640;
+const COMMUTING_MODES = ["driving", "driving-traffic", "walking", "cycling"];
 
 export default function Home() {
   const [sideNavOpen, setsideNavOpen] = useState<boolean>(false);
-  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+  const [windowWidth, setWindowWidth] = useState<number>(TAILWIND_SM_SCREEN);
 
   useEffect(() => {
     const handleResize = () => {
@@ -33,24 +35,41 @@ export default function Home() {
   }, [windowWidth]);
 
   return (
-    <main className="relative h-screen w-screen">
+    <main className="relative h-screen w-screen  ">
       <aside
-        className={`absolute top-10 left-10 z-10 rounded-xl bg-slate-50 flex flex-col ${
+        className={`absolute top-5 left-10 z-10 rounded-xl bg-primary-50 flex flex-col ${
           sideNavOpen ? "min-w-72 h-[90%] p-4 " : "w-fit h-fit p-0"
         }`}
       >
         <Switch
           checked={sideNavOpen}
           onChange={setsideNavOpen}
-          className="size-10 p-1.5 rounded-lg bg-slate-400"
+          className="size-10 p-1.5 rounded-lg bg-primary-200"
         >
           {sideNavOpen ? <Bars3BottomLeftIcon /> : <XMarkIcon />}
         </Switch>
         {
-          <section className={sideNavOpen ? "size-full" : "hidden"}>
-            <div>
-              <Input />
-            </div>
+          <section className={sideNavOpen ? "size-full py-10" : "hidden"}>
+            <form className="h-full">
+              <ul className="space-y-8">
+                <li className="">
+                  <Input />
+                </li>
+                <li>
+                  <RadioGroup className="space-y-2">
+                    {COMMUTING_MODES.map((commuting_mode) => (
+                      <Radio
+                        key={commuting_mode}
+                        value={commuting_mode}
+                        className="group relative flex cursor-pointer rounded-xl py-4 px-5 shadow-md bg-slate-50"
+                      >
+                        <p>{commuting_mode}</p>
+                      </Radio>
+                    ))}
+                  </RadioGroup>
+                </li>
+              </ul>
+            </form>
           </section>
         }
       </aside>
